168c2560323a64aa238010cf3550535844278647
Fábio Costa Camargo <122044234+FabioCCamarg@users.noreply.github.com>
2025-03-07 15:34:27 -0300

    Delete backend directory

--- backend/src/@types/express.d.ts
+++
@@ -1,8 +0,0 @@
-// src/@types/express.d.ts
-import 'express';
-
-declare module 'express' {
-    interface Request {
-        usuarioId?: number; // Adiciona a propriedade usuarioId ao objeto Request
-    }
-}
--- backend/src/routes/protegido.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-// Rota protegida
-router.get('/rota-protegida', (req, res) => {
-    res.json({ message: 'Rota protegida acessada com sucesso!'});
-});
-
-export default router;
